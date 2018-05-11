1fe6858dd8974d714f3692df7d27ed630e04dcc9
Jo Minkyu <alex9801@naver.com>
2018-05-12 06:11:59 +0900

    stats reflect DB

--- stats.js
+++ stats.js
@@ -4,121 +4,162 @@ $(document).ready(function() {
   // Firebase Database setup
   const database = firebase.database();
 
-  var groupRef = database.ref("group");
+  let groupRef = database.ref("group");
   groupRef.on("value", function(snapshot) {
+    $("#stats_nav_tab").empty();
+    $("#stats_nav_tabContent").empty();
+
+    let firstNav = true;
+
     snapshot.forEach(function(groupSnapshot) {
       let groupEntry = groupSnapshot.val();
-      
+      if(groupEntry.tracked) {
+        let title = groupEntry.title;
+        $("#stats_nav_tab").append(
+          `<a class="nav-item nav-link`
+          + (firstNav ? " active" : "") +
+          `" id="stats_nav_`
+          + title +
+          `_tab" data-toggle="tab" href="#stats_nav_`
+          + title +
+          `" role="tab" aria-controls="stats_nav_`
+          + title +
+          `" aria-selected="true" group="`
+          + title +
+          `">`
+          + title +
+          `</a>`
+        );
+        $("#stats_nav_tabContent").append(
+          `<div class="tab-pane fade`
+          + (firstNav ? " show active" : "") +
+          `" id="stats_nav_`
+          + title +
+          `" role="tabpanel" aria-labelledby="stats_nav_`
+          + title +
+          `_tab"><div class="chartArea" id="`
+          + "chartArea" + title +
+          `"></div></div>`
+        );
+
+        if(firstNav)
+          loadAndDraw(title);
+
+        firstNav = false;
+
+        $("#stats_nav_tab a:last-of-type").click(function(events) {
+          events.preventDefault();
+
+          loadAndDraw($(this).attr("group"));
+
+          $(this).tab("show");
+        });
+      }
     });
   });
 
+  function loadAndDraw(group) {
 
-  $("#stats_nav a").on("click", function(events) {
-    events.preventDefault();
+    const chartDiv = $("#chartArea" + group);
+    const defaultTickCount = 15;
+    const completeColor = "green";
+    const notneededColor = "grey";
 
+    // Load the Visualization API and the corechart package.
+    google.charts.load("current", {"packages": ["corechart"]});
 
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawChart);
 
-    $(this).tab("show");
-  });
+    // Callback that creates and populates a data table,
+    // instantiates the pie chart, passes in the data and
+    // draws it.
+    function drawChart() {
+      const tasksRef = database.ref("task").orderByChild("group").equalTo(group);
+      tasksRef.once("value", function(tasksSnapshot) {
+        let comCount = {
+          "Mon": 0,
+          "Tue": 0,
+          "Wed": 0,
+          "Thu": 0,
+          "Fri": 0,
+          "Sat": 0,
+          "Sun": 0,
+        };
+        let notCount = {
+          "Mon": 0,
+          "Tue": 0,
+          "Wed": 0,
+          "Thu": 0,
+          "Fri": 0,
+          "Sat": 0,
+          "Sun": 0,
+        };
 
-  const group = "Feed";
-  const defaultTickCount = 15;
-  const completeColor = "green";
-  const notneededColor = "grey";
-
-
-  // Load the Visualization API and the corechart package.
-  google.charts.load("current", {"packages": ["corechart"]});
-
-  // Set a callback to run when the Google Visualization API is loaded.
-  google.charts.setOnLoadCallback(drawChart);
-
-  // Callback that creates and populates a data table,
-  // instantiates the pie chart, passes in the data and
-  // draws it.
-  function drawChart() {
-    const tasksRef = database.ref("task").orderByChild("group").equalTo(group);
-    tasksRef.once("value", function(tasksSnapshot) {
-      let comCount = {
-        "Mon": 0,
-        "Tue": 0,
-        "Wed": 0,
-        "Thu": 0,
-        "Fri": 0,
-        "Sat": 0,
-        "Sun": 0,
-      };
-      let notCount = {
-        "Mon": 0,
-        "Tue": 0,
-        "Wed": 0,
-        "Thu": 0,
-        "Fri": 0,
-        "Sat": 0,
-        "Sun": 0,
-      };
-
-      tasksSnapshot.forEach(function(taskSnapshot) {
-        if(taskSnapshot.hasChild("status")) {
-          var status = taskSnapshot.child("status").val();
-          for(d in status) {
-            if(moment(d).isSameOrAfter(moment().subtract(7, "days"))) {
-              if(status[d])
-                comCount[moment(d).format("ddd")] += 1;
-              else
-                notCount[moment(d).format("ddd")] += 1;
+        tasksSnapshot.forEach(function(taskSnapshot) {
+          if(taskSnapshot.hasChild("status")) {
+            let status = taskSnapshot.child("status").val();
+            for(d in status) {
+              if(moment(d).isSameOrAfter(moment().subtract(7, "days"))) {
+                if(status[d])
+                  comCount[moment(d).format("ddd")] += 1;
+                else
+                  notCount[moment(d).format("ddd")] += 1;
+              }
             }
           }
+        });
+
+        // Create the data table.
+        let data = new google.visualization.DataTable();
+
+        data.addColumn("string", "Day");
+        data.addColumn("number", "Complete");
+        data.addColumn("number", "Not Needed");
+
+        comCount["Mon"] += 2;
+        notCount["Mon"] += 1;
+
+        let maxCount = 0;
+        for(let i = 1; i <= 7; i++) {
+          let day = moment().add(i, "days").format("ddd");
+          maxCount = Math.max(maxCount, comCount[day] + notCount[day]);
+          data.addRow([day, comCount[day], notCount[day]]);
         }
+
+        let tickCount = Math.min(maxCount + 1, defaultTickCount);
+        let tickMarks = [];
+        for(let i = 0; i < tickCount; i++)
+          tickMarks[i] = (i == tickCount - 1) ? maxCount : Math.floor(i * maxCount/ (tickCount - 1));
+
+        // Set chart options
+        let options = {
+          title: group + " (This week)",
+          isStacked: true,
+          vAxis: {
+            ticks: tickMarks,
+          },
+          series: {
+            0: {color: completeColor},
+            1: {color: notneededColor},
+          },
+          width: 700,
+          height: 300,
+        };
+
+        function selectHandler() {
+          let selectedItem = chart.getSelection()[0];
+          if (selectedItem) {
+            let day = data.getValue(selectedItem.row, 0);
+            alert("The user selected " + day);
+          }
+        }
+
+        // Instantiate and draw our chart, passing in some options.
+        let chart = new google.visualization.ColumnChart(chartDiv[0]);
+        google.visualization.events.addListener(chart, "select", selectHandler);
+        chart.draw(data, options);
       });
-
-      // Create the data table.
-      let data = new google.visualization.DataTable();
-
-      data.addColumn("string", "Day");
-      data.addColumn("number", "Complete");
-      data.addColumn("number", "Not Needed");
-
-      comCount["Mon"] += 2;
-      notCount["Mon"] += 1;
-
-      let maxCount = 0;
-      for(let i = 1; i <= 7; i++) {
-        let day = moment().add(i, "days").format("ddd");
-        maxCount = Math.max(maxCount, comCount[day] + notCount[day]);
-        data.addRow([day, comCount[day], notCount[day]]);
-      }
-
-      let tickCount = Math.min(maxCount + 1, defaultTickCount);
-      let tickMarks = [];
-      for(let i = 0; i < tickCount; i++)
-        tickMarks[i] = (i == tickCount - 1) ? maxCount : Math.floor(i * maxCount/ (tickCount - 1));
-
-      // Set chart options
-      let options = {
-        title: group + " (This week)",
-        isStacked: true,
-        vAxis: {
-          ticks: tickMarks,
-        },
-        series: {
-          0: {color: completeColor},
-          1: {color: notneededColor},
-        },
-      };
-
-      function selectHandler() {
-        let selectedItem = chart.getSelection()[0];
-        if (selectedItem) {
-          let day = data.getValue(selectedItem.row, 0);
-          alert("The user selected " + day);
-        }
-      }
-
-      // Instantiate and draw our chart, passing in some options.
-      let chart = new google.visualization.ColumnChart($("#chartArea")[0]);
-      google.visualization.events.addListener(chart, "select", selectHandler);
-      chart.draw(data, options);
-    });
+    }
   }
 });
